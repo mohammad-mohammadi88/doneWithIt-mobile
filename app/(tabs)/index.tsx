acd4af88845301e492fd9361f73bed59a2523ac8
mohammad-mohammadi88 <m.mohammadi8811@gmail.com>
2025-06-04 01:05:43 +0330

    Register, Login and ListingEdit forms and screens added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import AppScreen from '@Screens/AppScreen';
 // import AccountScreen from '@Screens/AccountScreen';
 // import ListingDetailsScreen from '@Screens/ListingDetailsScreen';
 // import ListingsScreen from '@Screens/ListingsScreen';
+// import ListingEditScreen from '@Screens/ListingEditScreen';
 import RegisterScreen from '@Screens/RegisterScreen';
 import type { FC } from 'react';
 
